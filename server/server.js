001a6e9b13b15bd901e21d459c96d14a274d8df8
Huayu <anubiss13477@gmail.com>
2017-05-08 02:08:15 -0700

    improve the mobile page

--- server/server.js
+++ server/server.js
@@ -1,62 +1,99 @@
-var gpio = require('onoff').Gpio,
-    RaspiCam = require('raspicam'),
-    led1 = new gpio(27, 'out'),
-    led2 = new gpio(22, 'out'),
-    buzzer = new gpio(12, 'out');
+var Gpio = require('onoff').Gpio,	//onoff module (use npm install onoff)
+    button = new Gpio(18, 'in', 'both'),	//setup GPIO17 as output
+    touch = new Gpio(12, 'in', 'both'),
+    led1 = new Gpio(20, 'out'),
+    led1State = 0,
+    level = 0,
+    timer = 0;
 
-var ledState = 0;
+var RgbChannel = require('rpi-rgb').Channel;
+var Colour = require('rpi-rgb').Colour;
+
+var channel1 = new RgbChannel(6,5,4);// BCM 25,24,23
+
+
+var blue = new Colour(0,100,0);
+var white = new Colour(100,100,100);
+var black = new Colour(0,0,0);
+
+button.setActiveLow( true );		//optional to reverse button value
+touch.setActiveLow( true );
+
+var pattern = [[100,100,100,100], [200,200,200,200], [300,300,300,300]];
+
+function funPattern(i){
+    setTimeout(function(){ led.writeSync( 1 ); }, pattern[i][0]);
+    setTimeout(function(){ led.writeSync( 0 ); }, pattern[i][1]);
+    setTimeout(function(){ led.writeSync( 1 ); }, pattern[i][2]);
+    setTimeout(function(){ led.writeSync( 0 ); }, pattern[i][3]);
+    console.log('level' + i );
+    level = level + 1;
+}
+
 
 process.env.NODE_URL='10.0.1.30';
 
 require('mahrio').runServer( process.env, __dirname ).then( function( server ) {
 
-  server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-      directory: {
-        path: ['../public/']
-      }
-    }
-  });
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: ['../public/']
+            }
+        }
+    });
 
-  var io = require('socket.io').listen( server.listener );
+    var io = require('socket.io').listen( server.listener );
+    io.on('connection', function( socket ) {
+        console.log('connection: ', socket.id );
+        socket.emit('event:hello');
+
+        //BEGIN LISTENING FOR SOCKET MESSAGES FROM CLIENTS
+        //Example:
+        button.watch(function(err, value) {	//watch button changes
+            if (value == true){
+
+                timer = timer + 1 ;
+                console.log('timer:'+ timer);
+
+                if ( timer % 2 == 0 ){
+                    console.log('Power  is off');
+                    channel1.fadeRgb(black,500);
+
+                }else{
+                    console.log('Power  is on');
+                    channel1.strobeRgb(white, 100, 1500);
+                    channel1.fadeRgb(blue,500);
+
+                    socket.emit('event:button', true);
+                    console.log("send power signal to phone")
+
+                }
+            }else{
+                //console.log('Power  is off');
+            }
+        });
 
 
 
-  io.on('connection', function( socket ) {
-    console.log('connection: ', socket.id );
-    socket.emit('event:hello');
+        //socket.on('myCustomMessage', function( val ){ console.log( val ); });
 
-      //LED
-      socket.on('event:light', function () {
-          console.log("turn on light ");
-          ledState = ledState + 1;
-          led1.writeSync(ledState%2);
-          led2.writeSync(ledState%2);
-      });
+    });
 
-      //Buzzer
-      socket.on('event:buzzer', function () {
-          buzzer.writeSync(1);
-          console.log("turn on buzzer ");
-          setTimeout(function () {
-              buzzer.writeSync(0);
-          }, 3000);
-      });
+    var state = false;
+    setInterval( function(){
+        io.sockets.emit('event:led:state', state = !state );
+    }, 1000);
 
-  
-    //BEGIN LISTENING FOR SOCKET MESSAGES FROM CLIENTS
-    //Example:
-    //socket.on('myCustomMessage', function( val ){ console.log( val ); });
+    console.log('Server Ready');
+});
 
-  });
-
-
-  var state = false;
-  setInterval( function(){
-    io.sockets.emit('event:led:state', state = !state );
-  }, 1000);
-
-  console.log('Server Ready');
+process.on('SIGINT', function(){
+    button.unexport();
+    touch.unexport();
+    led1.unexport();
+    led2.unexport();
+    process.exit();
 });
